a9bd103a80ece8f0b370caf2832c50cf26fd9e3d
Chen Ao <aochen@student.ethz.ch>
2020-06-08 19:15:39 +0200

    Update jacobian.py

--- jacobian.py
+++ jacobian.py
@@ -6,9 +6,9 @@ from functools import partial
 
 def extend(model, input_shape):
     if not isinstance(model, nn.Module):
-        raise TypeError('model should be a nn.Module')
+        raise TypeError("model should be a nn.Module")
     if not isinstance(input_shape, tuple):
-        raise TypeError('input_shape should be a tuple')
+        raise TypeError("input_shape should be a tuple")
 
     device = next(model.parameters()).device
 
@@ -36,19 +36,25 @@ def extend(model, input_shape):
                     Nweight = module.weight.numel()
                     weight_input = []
                     weight_output = []
-                    weight_repeat = torch.zeros(Nweight, dtype=torch.long, device=device)
-                    Xeye = torch.eye(x.numel(), device=device).reshape((-1,)+x.shape[1:])
+                    weight_repeat = torch.zeros(
+                        Nweight, dtype=torch.long, device=device
+                    )
+                    Xeye = torch.eye(x.numel(), device=device).reshape(
+                        (-1,) + x.shape[1:]
+                    )
                     for i in range(Nweight):
                         weight = torch.zeros(Nweight, device=device)
-                        weight[i] = 1.
+                        weight[i] = 1.0
                         module.weight.data = weight.reshape(module.weight.shape)
                         # output of module is of dimension (j,k)
                         out = module(Xeye).reshape(x.numel(), y.numel())
-                        if (out[out.abs()>1e-5] - 1.).abs().max() > 1e-5:
-                            raise RuntimeError('the network is not written in the standard form, see https://github.com/ChenAo-Phys/pytorch-Jacobian')
+                        if (out[out.abs() > 1e-5] - 1.0).abs().max() > 1e-5:
+                            raise RuntimeError(
+                                "the network is not written in the standard form, see https://github.com/ChenAo-Phys/pytorch-Jacobian"
+                            )
                         nonzero = torch.nonzero(out > 0.5, as_tuple=False)
-                        weight_input.append(nonzero[:,0])
-                        weight_output.append(nonzero[:,1])
+                        weight_input.append(nonzero[:, 0])
+                        weight_output.append(nonzero[:, 1])
                         weight_repeat[i] = nonzero.shape[0]
                     weight_input_list.append(torch.cat(weight_input, dim=0))
                     weight_output_list.append(torch.cat(weight_output, dim=0))
@@ -58,20 +64,22 @@ def extend(model, input_shape):
                     weight_input_list.append(None)
                     weight_output_list.append(None)
                     weight_repeat_list.append(None)
-                
+
                 if module.bias is not None:
                     Nbias = module.bias.numel()
                     bias_output = []
                     bias_repeat = torch.zeros(Nbias, dtype=torch.long, device=device)
                     for i in range(Nbias):
                         bias = torch.zeros(Nbias, device=device)
-                        bias[i] = 1.
+                        bias[i] = 1.0
                         module.bias.data = bias.reshape(module.bias.shape)
                         out = module(x).reshape(-1)
-                        if (out[out.abs()>1e-5] - 1.).abs().max() > 1e-5:
-                            raise RuntimeError('the network is not written in the standard form, see https://github.com/ChenAo-Phys/pytorch-Jacobian')
+                        if (out[out.abs() > 1e-5] - 1.0).abs().max() > 1e-5:
+                            raise RuntimeError(
+                                "the network is not written in the standard form, see https://github.com/ChenAo-Phys/pytorch-Jacobian"
+                            )
                         nonzero = torch.nonzero(out > 0.5, as_tuple=False)
-                        bias_output.append(nonzero[:,0])
+                        bias_output.append(nonzero[:, 0])
                         bias_repeat[i] = nonzero.shape[0]
                     bias_output_list.append(torch.cat(bias_output, dim=0))
                     bias_repeat_list.append(bias_repeat)
@@ -79,23 +87,34 @@ def extend(model, input_shape):
                 else:
                     bias_output_list.append(None)
                     bias_repeat_list.append(None)
-                    
+
             x = torch.zeros_like(y)
-        
-    if not hasattr(model, '_Jacobian_shape_dict'):
+
+    if not hasattr(model, "_Jacobian_shape_dict"):
         model._Jacobian_shape_dict = {}
     model._Jacobian_shape_dict[input_shape] = (
-        weight_input_list, weight_output_list, weight_repeat_list, bias_output_list, bias_repeat_list)
-
+        weight_input_list,
+        weight_output_list,
+        weight_repeat_list,
+        bias_output_list,
+        bias_repeat_list,
+    )
 
     # assign jacobian method to model
     def jacobian(self, as_tuple=False):
         shape = self.input_shape
-        if hasattr(self, '_Jacobian_shape_dict') and shape in self._Jacobian_shape_dict:
-            weight_input_list, weight_output_list, weight_repeat_list, bias_output_list, bias_repeat_list \
-            = self._Jacobian_shape_dict[shape]
+        if hasattr(self, "_Jacobian_shape_dict") and shape in self._Jacobian_shape_dict:
+            (
+                weight_input_list,
+                weight_output_list,
+                weight_repeat_list,
+                bias_output_list,
+                bias_repeat_list,
+            ) = self._Jacobian_shape_dict[shape]
         else:
-            raise RuntimeError('model or specific input shape is not extended for jacobian calculation')
+            raise RuntimeError(
+                "model or specific input shape is not extended for jacobian calculation"
+            )
 
         device = next(model.parameters()).device
         jac = []
@@ -109,14 +128,16 @@ def extend(model, input_shape):
                 bias_repeat = bias_repeat_list[layer]
                 x = self.x_in[layer]
                 N = x.shape[0]
-                dz_dy = self.gradient[layer].reshape(N,-1)
+                dz_dy = self.gradient[layer].reshape(N, -1)
 
                 if weight_repeat is not None:
                     Nweight = weight_repeat.shape[0]
                     dz_dy_select = dz_dy[:, weight_output]
-                    x_select = x.reshape(N,-1)[:, weight_input]
+                    x_select = x.reshape(N, -1)[:, weight_input]
                     repeat = torch.repeat_interleave(weight_repeat)
-                    dz_dW = torch.zeros(N,Nweight, device=device).index_add_(1, repeat, dz_dy_select * x_select)
+                    dz_dW = torch.zeros(N, Nweight, device=device).index_add_(
+                        1, repeat, dz_dy_select * x_select
+                    )
                     if as_tuple:
                         dz_dW = dz_dW.reshape((N,) + module.weight.shape)
                     jac.append(dz_dW)
@@ -124,7 +145,9 @@ def extend(model, input_shape):
                     Nbias = bias_repeat.shape[0]
                     dz_dy_select = dz_dy[:, bias_output]
                     repeat = torch.repeat_interleave(bias_repeat)
-                    dz_db = torch.zeros(N,Nbias, device=device).index_add_(1, repeat, dz_dy_select)
+                    dz_db = torch.zeros(N, Nbias, device=device).index_add_(
+                        1, repeat, dz_dy_select
+                    )
                     if as_tuple:
                         dz_db = dz_db.reshape((N,) + module.bias.shape)
                     jac.append(dz_db)
@@ -134,19 +157,16 @@ def extend(model, input_shape):
             return tuple(jac)
         else:
             return torch.cat(jac, dim=1)
-    
-    if not hasattr(model, 'jacobian'):
+
+    if not hasattr(model, "jacobian"):
         model.jacobian = types.MethodType(jacobian, model)
 
 
-    
-class JacobianMode():
-    
+class JacobianMode:
     def __init__(self, model):
         self.model = model
         if not isinstance(model, nn.Module):
-            raise TypeError('model should be a nn.Module')
-
+            raise TypeError("model should be a nn.Module")
 
     def __enter__(self):
         model = self.model
@@ -154,7 +174,7 @@ class JacobianMode():
         model.gradient = []
         self.forward_pre_hook = []
         self.backward_hook = []
-        
+
         def record_input_shape(self, input):
             model.input_shape = input[0].shape[1:]
 
@@ -172,10 +192,15 @@ class JacobianMode():
             if sum(p.numel() for p in module.parameters()):
                 model.x_in.append(None)
                 model.gradient.append(None)
-                self.forward_pre_hook.append(module.register_forward_pre_hook(partial(record_forward, layer=layer)))
-                self.backward_hook.append(module.register_backward_hook(partial(record_backward, layer=layer)))
+                self.forward_pre_hook.append(
+                    module.register_forward_pre_hook(
+                        partial(record_forward, layer=layer)
+                    )
+                )
+                self.backward_hook.append(
+                    module.register_backward_hook(partial(record_backward, layer=layer))
+                )
                 layer += 1
-
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.first_forward_hook.remove()
@@ -183,7 +208,7 @@ class JacobianMode():
             hook.remove()
         for hook in self.backward_hook:
             hook.remove()
-        
+
         del self.model.input_shape
         del self.model.x_in
         del self.model.gradient
